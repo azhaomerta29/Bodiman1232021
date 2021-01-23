b6a0b0c871f9bd76951c82cbc637d22eaa6b76f1
Azha Omerta <azhaomerta29@outlook.com>
2021-01-23 00:55:57 -0600

    Initial Commit
    Initial Commit for castles in England

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Castles In England";
+include ".\\lib\\header.php";
+?>
+
+<!-- MAIN CONTENT -->
+<h1>Castles in England</h1>
+<ul>
+    <li>Dover Castle</li>
+    <li>Bodiam Castle</li>
+    <li>Warwick Castle</li>
+    <li>Leeds Castle</li>
+</ul>
+
+<?php
+include ".\\lib\\footer.php";
+?>
